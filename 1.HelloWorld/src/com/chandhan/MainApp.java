69540e4f7f84e35bc83b26fb3736d6f5fb78825c
chandhan619 <62613173+chandhan619@users.noreply.github.com>
2021-02-03 22:56:39 +0530

    MainApp added

--- 1.HelloWorld/src/com/chandhan/MainApp.java
+++ 1.HelloWorld/src/com/chandhan/MainApp.java
@@ -3,7 +3,7 @@ package com.chandhan;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Hello World");
 
 	}
 
---
+++ 2.GitBackUp/src/com/chandhan/MainApp.java
@@ -0,0 +1,5 @@
+package com.chandhan;
+
+public class MainApp {
+
+}
